b5bded6eb1b7e8823d0d32a679311ec1bfd209b9
Vilyn1109 <victoria@thelayfamily.net>
2025-03-11 20:50:38 -0500

    2025-03-11_HiddenCalculatorWords

---
+++ 2025-03-11_HiddenCalculatorWords.py
@@ -0,0 +1,57 @@
+# goal : Act like you are entering numbers into a calculator, then flipping it.
+
+def HiddenCalculatorWords() :
+    
+    # Dictionary of numbers to letters.
+
+    d_Words = \
+        {
+            1 : 'I',
+            2 : 'Z',
+            3 : 'E',
+            4 : 'H',
+            5 : 'S',
+            6 : 'G',
+            7 : 'L',
+            8 : 'B',
+            0 : 'O'
+         }
+    
+    # Get input and initilize Letters string.
+
+    print ("-----")
+    v_input = input ( )
+
+    v_Letters = ""
+
+    # Make sure input is only integers.
+
+    for v_Character in v_input :
+
+        try :
+            int(v_Character)
+        except :
+            print ("Invalid character in input.")
+            return
+
+        # Make sure input is in dictionary, if so add it to letters string.
+
+        v_Character = int(v_Character)
+        if v_Character not in d_Words :
+            print ("Invalid character in input.")
+            return
+        
+        else :
+            v_Letters += d_Words[int(v_Character)]
+        
+    # Reverse the string.
+
+    v_result = v_Letters[::-1]
+    print("-----")
+    print(v_result)
+
+    return
+
+if __name__ == "__main__" :
+
+    HiddenCalculatorWords()
